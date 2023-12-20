b798ccdeaf35fdbcd16b585f457a5f7f66edf83d
Cassim <cassim.ali@outlook.fr>
2023-12-20 01:15:51 -0500

    Update cartNumbers function : -Add if statement (item exist or not)

--- scripts/main.js
+++ scripts/main.js
@@ -54,22 +54,31 @@ let products = [
     }
 ]
 
-/** procédure qui compte le nombre d'item dans le panier à chaque evenement click sur .add-cart element*/
+/**
+ * procédure qui compte le nombre d'item dans le panier 
+ * à chaque evenement click sur .add-cart element
+ */
 for(let i=0;i < carts.length; i++){
     carts[i].addEventListener('click',() =>{
         cartNumbers();
     })
 }
-/** Methode qui compte le nombre d'item ajouter au panier */
+
+/**
+ * fonction qui va compter le nombre d'item ajouter au panier sous conditions.
+ */
 function cartNumbers(){
-
-    let productNumbers = localStorage.getItem('cartCount'); // récupérer l'objet stocké dans localStorage
-    console.log(productNumbers);
-    console.log(typeof productNumbers);
-
-    productNumbers  = parseInt(productNumbers);
-    console.log(productNumbers);
-    console.log(typeof productNumbers);
-    localStorage.setItem('cartCount', 1); // enregistre les element click dans localStorage
-
+    let productNumbers = localStorage.getItem('cartNumbers'); // récupérer l'objet stocké dans localStorage
+    
+    productNumbers  = parseInt(productNumbers); // traduire en entier la string productNumbers
+    //Comptez le nombre d'items ajouter au panier et sauvegarder dans localStorage
+    if(productNumbers){ // si item dans panier
+        localStorage.setItem('cartNumbers', productNumbers + 1); // enregistrer item dans localStorage
+        document.querySelector('.count').textContent = productNumbers + 1; // augmenter le nombre d'item de 1 sur l'icon cart dans navbar
+    }else{ // Si panier vide 
+        localStorage.setItem('cartNumbers', 1); // enregistre l'element cliqué dans localStorage
+        document.querySelector('.count').textContent = 1; // augmente le nombre d'item a 1 sur l'icon cart dans navbar
+    }
 }
+// Appel de la fonction au chargement de la page.
+loadCartNumbers() 
