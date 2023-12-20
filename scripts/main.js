a4d9cbca2578735b273bc9adb85d32a2fa9035a5
Cassim <cassim.ali@outlook.fr>
2023-12-20 10:33:18 -0500

    Get the items data and save in localStorage

--- scripts/main.js
+++ scripts/main.js
@@ -75,7 +75,7 @@ let products = [
  */
 for(let i=0;i < carts.length; i++){
     carts[i].addEventListener('click',() =>{
-        cartNumbers();
+        cartNumbers(products[i]); // Appel de la fonction cartNumbers auquel on passe un argument product[i]
     })
 }
 /**
@@ -83,16 +83,16 @@ for(let i=0;i < carts.length; i++){
  * l'icone panier si existe
  */
 function loadCartNumbers(){
-    let productNumbers = localStorage.getItem('cartNumbers');
+    let productNumbers = localStorage.getItem('cartNumbers'); //récupère les objets ayant comme propriétés cartNumbers du localStorage
     if(productNumbers){
-        document.querySelector('.count').textContent = productNumbers;
+        document.querySelector('.count').textContent = productNumbers; // Selectionne le contenu texte du 1er element avec .count class et on y insere productNumbers
     }
 };
 
 /**
  * fonction qui va compter le nombre d'item ajouter au panier sous conditions.
  */
-function cartNumbers(){
+function cartNumbers(product){
     let productNumbers = localStorage.getItem('cartNumbers'); // récupérer l'objet stocké dans localStorage
     
     productNumbers  = parseInt(productNumbers); // traduire en entier la string productNumbers
@@ -104,6 +104,37 @@ function cartNumbers(){
         localStorage.setItem('cartNumbers', 1); // enregistre l'element cliqué dans localStorage
         document.querySelector('.count').textContent = 1; // augmente le nombre d'item a 1 sur l'icon cart dans navbar
     }
+
+    setItems(product);// appel de la fonction setItems()
+}
+/**
+ * fonction qui récupére les données des produits cliqués,
+ * et qui les sauvegarde dans localStorage.
+ */
+function setItems(product){
+    let cartItems = localStorage.getItem("productsInCart");// récupère les objets ayant comme propriétés productInCart du localStorage
+    cartItems = JSON.parse(cartItems); // convertit cartItems en objet js
+    console.log("My cartItems are", cartItems);
+    if(cartItems != null){ // si panier n'est pas vide
+        // gestion error Cannot read properties of undefined (reading 'inCart')
+        if(cartItems[product.tag] == undefined){ //Si la propriété tag de product object est undefined
+            cartItems = { // ajouter le produit nouvellement cliqué au panier déjà existant
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1; // augmente la valeur de InCart de 1
+    }else{ // si panier vide
+        product.inCart = 1; // affecte la valeur 1 à la propriété incart de products 
+        cartItems = {
+            [product.tag]: product
+        }
+
+    }
+
+    //créer un objet js avec pour key name 'productInCart' et l'objet js cartItems comme key value qu'on convertit en JSON string
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems)); 
+
 }
 // Appel de la fonction au chargement de la page.
 loadCartNumbers() 
